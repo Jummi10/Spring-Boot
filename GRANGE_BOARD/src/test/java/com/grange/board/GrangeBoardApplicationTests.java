3c9a4524a6efd719185df0ef96f9692f6d992557
jummi <98qkrwjdal@naver.com>
2020-09-20 18:11:45 +0900

    create Login form

--- GRANGE_BOARD/src/test/java/com/grange/board/GrangeBoardApplicationTests.java
+++ GRANGE_BOARD/src/test/java/com/grange/board/GrangeBoardApplicationTests.java
@@ -1,5 +1,7 @@
 package com.grange.board;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.h2.security.SHA256;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class GrangeBoardApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		String pw = "test1234";
+		System.out.println("original pw : " + pw);
+		
+		String encPw = DigestUtils.sha256Hex(pw);
+		System.out.println("encyrpted pw : " + encPw);
 	}
 
 }
