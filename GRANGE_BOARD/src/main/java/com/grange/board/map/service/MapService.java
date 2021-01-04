fc9bcf8e9c24d994058ae342f3e65d9c5f9b2143
jummi <98qkrwjdal@naver.com>
2021-01-05 02:20:12 +0900

    Show marker at position clicked on kakao map

---
+++ GRANGE_BOARD/src/main/java/com/grange/board/map/service/MapService.java
@@ -0,0 +1,20 @@
+package com.grange.board.map.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.grange.board.map.dao.MapDao;
+import com.grange.board.map.vo.MapVO;
+
+@Service
+public class MapService {
+
+	@Autowired
+	private MapDao mapDao;
+
+	public List<MapVO> getMaps() {
+		return mapDao.getMaps();
+	}
+}
---
+++ GRANGE_BOARD/src/main/java/com/grange/board/map/vo/MapVO.java
@@ -0,0 +1,57 @@
+package com.grange.board.map.vo;
+
+import java.util.Date;
+
+public class MapVO {
+	
+	private int id;
+	private String comment;
+	private Date regDtt;
+	private String regId;
+	private double latitude;
+	private double longitude;
+	private String useYn;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public Date getRegDtt() {
+		return regDtt;
+	}
+	public void setRegDtt(Date regDtt) {
+		this.regDtt = regDtt;
+	}
+	public String getRegId() {
+		return regId;
+	}
+	public void setRegId(String regId) {
+		this.regId = regId;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public String getUseYn() {
+		return useYn;
+	}
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
+}
